75fae545549ba2b3cb1ef09e1d0f6f8655a196e2
DESKTOP-3DC4NTH\nguye <nguyenvantiep15112002@gmail.com>
2023-12-23 15:35:27 +0700

    abc

--- J6D8/src/main/java/com/j6d8/controller/AuthController.java
+++ J6D8/src/main/java/com/j6d8/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.j6d8.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.j6d8.service.impl.UserServiceImpl;
@@ -17,5 +18,10 @@ public class AuthController {
     public String success(OAuth2AuthenticationToken oauth2) {
         impl.loginFormOAuth2(oauth2);
         return "forward:/security/login/success";
+    }
+
+    @GetMapping("/tesst")
+    public String view(){
+        return "";
     }
 }
